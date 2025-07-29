f24cc9508bea11ee1cf5cb3511934f1df44114e3
brentjm <brent_maranzano@yahoo.com>
2025-07-29 18:14:34 -0400

    Created final version to show.

--- export_gltf.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-FILE = "digital_twin"
-
-bpy.ops.wm.open_mainfile(filepath=FILE + ".blend")
-bpy.ops.export_scene.gltf(
-    filepath=FILE + ".glb", export_format="GLB", export_apply=True
-)
